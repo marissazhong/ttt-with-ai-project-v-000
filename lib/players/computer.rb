89342d9639d482df3bea1e38491c6337cf1595b3
Marissa Zhong <mwz2101@columbia.edu>
2018-07-02 20:09:25 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -24,8 +24,8 @@ module Players
         other_player = (all_moves(board)[0].length == all_moves(board)[1].length ? "O" : "X")
         current_check_values = [" ",current_player]
         other_check_values = [" ",other_player]
+        other_player_closest = [0,"combo"] # [how many spots occupied towards a solution, closest combo to a solution]
         Game::WIN_COMBINATIONS.map {|combo|
-          other_player_closest = [0,"combo"] # [how many spots occupied towards a solution, closest combo to a solution]
           if current_check_values.include?(board.cells[combo[0]]) && current_check_values.include?(board.cells[combo[1]]) && current_check_values.include?(board.cells[combo[2]])
             current_player_valid_combos << combo
           elsif other_check_values.include?(board.cells[combo[0]]) && other_check_values.include?(board.cells[combo[1]]) && other_check_values.include?(board.cells[combo[2]])
@@ -33,7 +33,6 @@ module Players
             other_player_moves = 0
             combo.each {|position| other_player_moves += 1 if board.cells[position] == other_player }
             if other_player_moves > other_player_closest[0]
-              other_player_closest = [0,0]
               other_player_closest[0] = other_player_moves
               other_player_closest[1] = combo
             end
