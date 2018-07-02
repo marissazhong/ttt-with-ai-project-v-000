f44e94dfdcf9b56b46c714c09ee6a475b2a8727f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-02 20:43:13 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -45,6 +45,7 @@ module Players
           if index.even? && index != 4
             other_player_closest[1][1] + 1
           elsif index == 4
+            puts [0,2,6,8].select {|index| board.valid_move?(index)}.sample + 1
             [0,2,6,8].select {|index| board.valid_move?(index)}.sample + 1
           else
             [other_player_closest[1][0],other_player_closest[1][2]].sample + 1
